3ebce466d470549fc6b2e0245d20030693daa8ee
Denise <denise.lim.sh@gmail.com>
2019-11-06 13:42:49 +0800

    #3 Separate service contract module to hold contracts

---
+++ service-contract/src/main/java/com/thekneeslim/contract/Book.java
@@ -0,0 +1,18 @@
+package com.thekneeslim.contract;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+public class Book {
+
+    private final long id;
+    private final String title;
+    private final String description;
+    private final String author;
+    private final Instant publishingDate;
+
+}
